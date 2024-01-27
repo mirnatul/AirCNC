4eae8e7924e9a8b2e2bf1b238573377978f94fa9
mirnatul <www.mirnatul@gmail.com>
2024-01-27 17:11:10 +0600

    category box design completed

--- client/src/routes/Routes.jsx
+++ client/src/routes/Routes.jsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from 'react-router-dom'
+import Home from '../pages/Home';
 import Main from './../layouts/Main'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Main></Main>
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      }
+    ]
   },
 ])
 
